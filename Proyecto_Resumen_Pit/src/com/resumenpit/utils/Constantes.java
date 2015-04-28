9a45f337df96b385208f13debe3be7efc4912fae
Sebastian <cristhianp00@gmail.com>
2015-04-28 00:58:43 -0500

    Agregando constantes

--- Proyecto_Resumen_Pit/src/com/resumenpit/utils/Constantes.java
+++ Proyecto_Resumen_Pit/src/com/resumenpit/utils/Constantes.java
@@ -5,4 +5,5 @@ public class Constantes
 
 	public final static String MENSAJE_REGISTRO_EXITOSO="REGISTRO EXITOSO";
 	public final static String MENSAJE_REGISTRO_ERROR="DATOS INVALIDOS DEL LOGIN";
+	public final static String MENSAJE_ERROR_DATOS="ERROR CON LOS DATOS INGRESADOS";
 }
