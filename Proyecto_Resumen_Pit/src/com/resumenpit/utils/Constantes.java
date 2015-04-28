53843bf36a754b88be93ee475c4263ef97524e21
Sebastian <asefora2080@gmail.com>
2015-04-28 14:42:55 -0500

    Agregando comentarios

--- Proyecto_Resumen_Pit/src/com/resumenpit/utils/Constantes.java
+++ Proyecto_Resumen_Pit/src/com/resumenpit/utils/Constantes.java
@@ -2,7 +2,7 @@ package com.resumenpit.utils;
 
 public class Constantes 
 {
-        //Agregando
+    //Constantes: se especifica final para que no pueda ser alterado
 	public final static String MENSAJE_REGISTRO_EXITOSO="REGISTRO EXITOSO";
 	public final static String MENSAJE_REGISTRO_ERROR="DATOS INVALIDOS DEL LOGIN";
 	public final static String MENSAJE_ERROR_DATOS="ERROR CON LOS DATOS INGRESADOS";
